7a19d6f29b20be14ff4fe4698ceace4309f85042
Dan Cai <zxc61013@gmail.com>
2022-12-12 18:06:28 +0800

    交大課程02

---
+++ LAB_02/task_2_1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "I am Dan. \nMy age is 27. " << endl;
+    cout << "I am Dan. << enld << My age is 27. ";
+    
+    return 0;
+}
