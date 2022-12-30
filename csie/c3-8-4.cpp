d5dbaecf63c77f61a0bcfdada6f4180685b9109f
Dan Cai <zxc61013@gmail.com>
2022-12-30 18:12:09 +0800

    error

--- csie/c3-8-4.cpp
+++ csie/c3-8-4.cpp
@@ -3,12 +3,29 @@ using namespace std;
 
 int main()
 {
-    int n, t;
+    int N, T;
     int no[99];
     int i;
+    int n, t, m;
 
-    cin >> n >> t;
+    cin >> N >> T;
+    i = 0;
     
+    while (i <= T)
+    {
+        cin >> n, m, t;
+        no[n] = 0;
+
+        if (t == 1)
+        {
+            no[n] += t;
+        }
+        else if (t == 2)
+        {
+            no[n] -= t;
+        }
+        cout << n << " : " << no[n] << endl;
+    }
 
     return 0;
 }
